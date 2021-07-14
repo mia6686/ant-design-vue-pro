c5b1a6ce442c884c8f26037e24ab7068c2b89a47
DESKTOP-PDI1654\LM <1207140306@qq.com>
2021-07-14 16:50:48 +0800

    mock 模拟数据 目前是显示500的报错信息

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -7,36 +7,75 @@
 <script>
 import random from "lodash/random";
 import Chart from "../../components/Chart.vue";
+//使用mock模拟数据的时候需要先引入axios
+import axios from "axios";
 
 export default {
     data() {
         return {
             chartOption: {
-                title: {
-                    text: "Echart 入门示例",
-                },
-                tooltip: {},
-                xAxis: {
-                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-                },
-                yAxis: {},
-                series: [
-                    {
-                        name: "销量",
-                        type: "bar",
-                        data: [5, 20, 36, 10, 10, 20],
-                    },
-                ],
+                //当模拟数据的时候这个数据就不能放在这里写死
+                // title: {
+                //     text: "Echart 入门示例",
+                // },
+                // tooltip: {},
+                // xAxis: {
+                //     data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+                // },
+                // yAxis: {},
+                // series: [
+                //     {
+                //         name: "销量",
+                //         type: "bar",
+                //         data: [5, 20, 36, 10, 10, 20],
+                //     },
+                // ],
             },
         };
     },
     mounted() {
+        //模拟数据的时候也不能这样去写
+        // this.interval = setInterval(() => {
+        //     //100以内的随机数更改这里面的值
+        //     this.chartOption.series[0].data = this.chartOption.series[0].data.map(() => random(100));
+        //     // 手动给option附一个新值 这样的话也是可以达到深度监听
+        //     this.chartOption = { ...this.chartOption };
+        // }, 3000);
+        //应该是这样写
+        //调用这个接收的数据
+        //接口请求已经写好了
+        //这个时候就需要去建立一个mock文件
+        this.getChartData();
+        //
         this.interval = setInterval(() => {
-            //100以内的随机数更改这里面的值
-            this.chartOption.series[0].data = this.chartOption.series[0].data.map(() => random(100));
-            // 手动给option附一个新值 这样的话就没问题了
-            this.chartOption = { ...this.chartOption };
+            this.getChartData();
         }, 3000);
+    },
+    //在方法里面执行数据的模拟
+    methods: {
+        //接收数据名
+        getChartData() {
+            axios.get("/api/sashboard/chart", { params: { ID: 12345 } }).then((response) => {
+                this.chartOption = {
+                    title: {
+                        text: "Echart 入门示例",
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+                    },
+                    yAxis: {},
+                    series: [
+                        {
+                            name: "销量",
+                            type: "bar",
+                            // data: [5, 20, 36, 10, 10, 20], 换成接收到的数据
+                            data: response.data,
+                        },
+                    ],
+                };
+            });
+        },
     },
     //使用之后需要进行销毁
     beforeDestroy() {
